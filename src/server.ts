68480918868424efc07d949ae9860b6cd45cbf2d
Paulo <paulo@pop-os.localdomain>
2021-06-17 20:21:01 -0300

    websocket basic confs

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
+
 import './database';
 
 import { routes } from './routes';
 
 const app = express();
+
+const http = createServer(app); // Criando protocolo http
+const io = new Server(http); // Criando protocolo ws (websocket)
 
 app.use(express.json());
 app.use(routes);
